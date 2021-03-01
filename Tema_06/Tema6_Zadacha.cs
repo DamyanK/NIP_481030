0d12236603ef47bbe36277a893b33942e88f673f
unknown <damqnkushev03@gmail.com>
2021-03-01 18:08:18 +0200

    Zadacha 6

---
+++ Tema_06/Tema6_Zadacha.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            do
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+            } while (N < 0 || N % 2 != 0);
+
+            int[] array;
+            do
+            {
+                array = Console.ReadLine()
+                    .Split(' ')
+                    .Select(x => Convert.ToInt32(x))
+                    .ToArray();
+            } while (array.Length != N);
+
+            int[] firstHalf = array.Take(N / 2).ToArray();
+            int[] secondHalf = array.Skip(N / 2).Take(N / 2).ToArray();
+
+            Console.WriteLine(String.Join(" ", firstHalf.OrderBy(x => x)) + " " +
+                String.Join(" ", secondHalf.OrderByDescending(x => x)));
+        }
+    }
+}
