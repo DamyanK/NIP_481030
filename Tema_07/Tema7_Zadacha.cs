e4603dd9baf3fedb71dec2cde3625bf29f9500b3
unknown <damqnkushev03@gmail.com>
2021-03-01 22:23:22 +0200

    Zadacha 7

---
+++ Tema_07/Tema7_Zadacha.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema7_Zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = int.Parse(Console.ReadLine());
+            int count = 0;
+            int[] notes = { 1, 5, 10, 20, 100 };
+
+            for (int i = notes.Length - 1; i >= 0; i--)
+            {
+                if (amount >= notes[i] && amount != 0)
+                {
+                    count += amount / notes[i];
+                    amount %= notes[i];
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
