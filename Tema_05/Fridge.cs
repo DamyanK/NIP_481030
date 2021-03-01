4aa95c9d2de1b7d9a183563b2a461676f5bd5238
unknown <damqnkushev03@gmail.com>
2021-03-01 17:58:20 +0200

    Problem with adding after removing

---
+++ Tema_05/Fridge.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema5_Zadacha
+{
+    class Fridge
+    {
+        private class Product
+        {
+            public string Name { get; set; }
+            public Product next;
+
+            public Product(string name)
+            {
+                this.Name = name;
+            }
+
+            public override string ToString()
+            {
+                return this.Name;
+            }
+        }
+
+        private Product head;
+        private Product tail;
+        private int count = 0;
+
+        public int GetLength()
+        {
+            return count;
+        }
+
+        private bool isEmpty()
+        {
+            return head == null;
+        }
+
+        public void AddProduct(string ProductName)
+        {
+            Product temp = new Product(ProductName);
+
+            if (isEmpty())
+            {
+                head = tail = temp;
+                count++;
+            }
+            else
+            {
+                tail.next = temp;
+                tail = temp;
+                count++;
+            }
+        }
+
+        private int IndexOf(string prod)
+        {
+            Product temp = head;
+            int index = 0;
+
+            while (temp != null)
+            {
+                if (temp.Name == prod)
+                {
+                    return index;
+                }
+
+                temp = temp.next;
+                index++;
+            }
+
+            return -1;
+        }
+
+        public bool CheckProductIsInStock(string name)
+        {
+            return IndexOf(name) != -1;
+        }
+
+        private Product getPrevious(Product prod)
+        {
+            Product temp = head;
+            while (temp.next != prod)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public string RemoveProductByIndex(int index)
+        {
+            if (index > count || isEmpty())
+            {
+                return null;
+            }
+            else
+            {
+                if (head == tail)
+                {
+                    string removed = head.Name;
+                    head = tail = null;
+                    count = 0;
+
+                    return removed;
+                }
+                if (index == 0)
+                {
+                    string removed = head.Name;
+                    Product second = head.next;
+                    head.next = null;
+                    head = second;
+
+                    return removed;
+                }
+                else // Това може да даде проблем
+                {
+                    Product temp = head;
+
+                    for (int i = 0; i < index; i++)
+                    {
+                        temp = temp.next;
+                    }
+
+                    string removed = temp.Name;
+                    Product previous = getPrevious(temp), next = temp.next;
+                    previous.next = next;
+                    temp = null;
+
+                    count--;
+
+                    return removed;
+                }
+            }
+        }
+
+        public string RemoveProductByName(string name)
+        {
+            return RemoveProductByIndex(IndexOf(name));
+        }
+
+        public string[] CookMeal(int start, int end)
+        {
+            string[] cooked = new string[end - start + 1];
+            Product temp = head;
+
+            for (int i = 0; i < start; i++)
+            {
+                temp = temp.next;
+            }
+
+            int index = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (temp != null)
+                {
+                    cooked[index] = temp.Name;
+                    temp = temp.next;
+                    index++;
+                }
+            }
+
+            return cooked;
+        }
+
+        public string[] ProvideInformationAboutAllProducts()
+        {
+            string[] products = new string[count];
+            Product temp = head;
+            int index = 0;
+
+            while (temp != null)
+            {
+                products[index] = temp.ToString();
+                temp = temp.next;
+                index++;
+            }
+
+            return products;
+        }
+    }
+}
---
+++ Tema_05/Tema5_Zadacha.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema5_Zadacha
+{
+    class Program
+    {
+        static Fridge fridge = new Fridge();
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            while (line != "END")
+            {
+                string[] cmdArgs = line.Split(' ');
+                switch (cmdArgs[0])
+                {
+                    case "Add":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        fridge.AddProduct(cmdArgs[1]);
+                        break;
+                    case "Check":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        if (fridge.CheckProductIsInStock(cmdArgs[1]))
+                        {
+                            Console.WriteLine("Product {0} is in stock.", cmdArgs[1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not in stock");
+                        }
+                        break;
+                    case "Remove":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        try
+                        {
+                            int index = int.Parse(cmdArgs[1]);
+                            Console.WriteLine(fridge.RemoveProductByIndex(index));
+                        }
+                        catch (FormatException e)
+                        {
+                            if (e.Data != null)
+                            {
+                                Console.WriteLine(fridge.RemoveProductByName(cmdArgs[1]));
+                            }
+                        }
+                        break;
+                    case "Print":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        string[] prods = fridge.ProvideInformationAboutAllProducts();
+                        foreach (var item in prods)
+                        {
+                            if (item != null)
+                            {
+                                Console.WriteLine("Product {0}", item);
+                            }
+                        }
+                        break;
+                    case "Cook":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        int start = Convert.ToInt32(cmdArgs[1]);
+                        int end = Convert.ToInt32(cmdArgs[2]);
+
+                        if (start < 0)
+                        {
+                            start = 0;
+                        }
+                        if (end > fridge.GetLength())
+                        {
+                            end = fridge.GetLength();
+                        }
+
+                        string[] cooked = fridge.CookMeal(start, end);
+                        Console.WriteLine("Meal cooked. Used Products: " + String.Join(", ", cooked));
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
