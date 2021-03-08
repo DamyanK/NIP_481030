b23e0ac7fee91c6570c89db879c514da12b129d6
unknown <damqnkushev03@gmail.com>
2021-03-08 11:38:24 +0200

    maybe fix switch later

---
+++ Tema_10/Tema10_Zadacha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema10_Zadacha
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var instruction = new Instructions();
+            string code = Console.ReadLine();
+            while (code != "END")
+            {
+                Console.WriteLine(instruction.Execute(code));
+                code = Console.ReadLine();
+            }
+        }
+    }
+}
